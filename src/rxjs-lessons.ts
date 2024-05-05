395d940fd1731db7617050afb132f158c4aaca85
Azamat Abdullin <usman.a.a.v@gmail.com>
2024-05-05 09:34:56 +0200

    stage2: implemented unsubscribe

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,27 +1,57 @@
-import { Observable, debounceTime, distinctUntilChanged, fromEvent, map } from "rxjs";
+import { Observable, debounceTime, distinctUntilChanged, filter, fromEvent, map, takeUntil } from "rxjs";
 
-const search$ = new Observable<Event>(observer => {
-    const searchInput = document.getElementById('search');
+// const search$ = new Observable<Event>(observer => {
+//     const searchInput = document.getElementById('search');
+//     const stopButton = document.getElementById('stop');
 
-    if (!searchInput) {
-        return observer.error('Element does not exist on the page')
-    }
+//     if (!searchInput || !stopButton) {
+//         return observer.error('Element does not exist on the page')
+//     }
 
-    searchInput?.addEventListener('input', (event) =>  {
-        // @ts-ignore
-        observer.next(event);
-    })
-});
+//     const clear = () => {
+//         searchInput.removeEventListener('input', handleSearch);
+//         stopButton.removeEventListener('click', handleStop);
+//     }
+
+//     const checkSubscription = () => {
+//         if (observer.closed) {
+//             clear();
+//         }
+//     }
+
+//     const handleSearch = (event: Event) =>  {
+//         checkSubscription();
+//         console.log(123);
+//         // @ts-ignore
+//         observer.next(event);
+//     };
+
+//     const handleStop = () => {
+//         checkSubscription();
+//         observer.complete();
+//         clear();
+//     }
+
+//     searchInput.addEventListener('input', handleSearch);
+//     stopButton.addEventListener('click', handleStop);
+// });
 
 const searchFromEvent$ = fromEvent<Event>(
     document.getElementById('search')!,
     'input',
-)
+);
 
-searchFromEvent$
+const stopFromEvent$ = fromEvent<Event>(
+    document.getElementById('stop')!,
+    'click',
+);
+
+const subscription = searchFromEvent$
     .pipe(
+        takeUntil(stopFromEvent$),
         debounceTime(300),
         map(event => (event.target as HTMLInputElement).value),
+        filter(value => value.length > 3 || !value),
         distinctUntilChanged(),
     )
     .subscribe({
@@ -32,4 +62,14 @@ searchFromEvent$
         complete: ()  => {
             console.log('Event completed')
         }
-    })
+    });
+
+// const stopSubscription = stopFromEvent$.subscribe(() => {
+//     subscription.unsubscribe();
+//     stopSubscription.unsubscribe();
+// });
+
+// setTimeout(() => {
+//     console.log('unsubscribed');
+//     subscription.unsubscribe();
+// }, 10000);
